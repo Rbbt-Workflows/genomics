773919d999fcf9ac492c0422bfb4e88bc12c71d1
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-07-10 14:38:52 +0200

    nil

--- workflow.rb
+++ workflow.rb
@@ -12,81 +12,93 @@ module Genomics
   extend Workflow
 
   input :tsv, :tsv, "TSV file to name", nil, :stream => true
-  task :names => :tsv do |tsv|
+  input :field, :string, "Field name for lists", nil
+  task :names => :tsv do |tsv,field|
     tsv = TSV::Parser.new tsv if IO === tsv
 
     named = TSV::Dumper.new tsv.options, path
     named.init
 
-    case tsv.type
-    when :single
-      TSV.traverse tsv, :into => named do |k,value|
-        k = k.first if Array === k
-        k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
-        k = k.name if k.respond_to? :name
-
-        value = Misc.prepare_entity(value, tsv.fields.first) if tsv.fields
-        value = value.name if value.respond_to? :name
-
-        [k,value]
+    if (tsv.fields.nil? or tsv.fields.empty?) and field
+      keys = []
+      TSV.traverse tsv, :type => :keys do |k|
+        keys << k
       end
-    when :list
-      TSV.traverse tsv, :into => named do |k,list|
-        k = k.first if Array === k
-        k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
-        k = k.name if k.respond_to? :name
+      keys = Misc.prepare_entity(keys, field)
+      keys.extend AnnotatedArray
+      keys.name
+    else
 
-        i = 0
-        values = list.collect do |value|
-          begin
-            value = Misc.prepare_entity(value, tsv.fields[i]) if tsv.fields
-            value = value.name if value.respond_to? :name
-            value
-          ensure
-            i += 1
-          end
+      case tsv.type
+      when :single
+        TSV.traverse tsv, :into => named do |k,value|
+          k = k.first if Array === k
+          k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
+          k = k.name if k.respond_to? :name
+
+          value = Misc.prepare_entity(value, tsv.fields.first) if tsv.fields
+          value = value.name if value.respond_to? :name
+
+          [k,value]
         end
-        [k,values]
-      end
-    when :flat
-      TSV.traverse tsv, :into => named do |k,values|
-        k = k.first if Array === k
-        k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
-        k = k.name if k.respond_to? :name
+      when :list
+        TSV.traverse tsv, :into => named do |k,list|
+          k = k.first if Array === k
+          k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
+          k = k.name if k.respond_to? :name
 
-        values = Misc.prepare_entity(values, tsv.fields.first) if tsv.fields
-        begin
-          values = values.name if values.respond_to? :name
-        rescue
-          Log.exception $!
-        end
-
-        [k,values]
-      end
-    when :double
-      fields = tsv.fields.dup if tsv.fields
-      TSV.traverse tsv, :into => named do |k,values_list|
-        k = k.first if Array === k
-        k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
-        k = k.name if k.respond_to? :name
-
-        if fields
           i = 0
-          new_value_list = values_list.collect do |values|
+          values = list.collect do |value|
             begin
-              values = Misc.prepare_entity(values, fields[i]) 
-              if values.respond_to? :name
-                values.name
-              else
-                values
-              end
+              value = Misc.prepare_entity(value, tsv.fields[i]) if tsv.fields
+              value = value.name if value.respond_to? :name
+              value
             ensure
               i += 1
             end
           end
-          [k,new_value_list]
-        else
-          [k,values_list]
+          [k,values]
+        end
+      when :flat
+        TSV.traverse tsv, :into => named do |k,values|
+          k = k.first if Array === k
+          k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
+          k = k.name if k.respond_to? :name
+
+          values = Misc.prepare_entity(values, tsv.fields.first) if tsv.fields
+          begin
+            values = values.name if values.respond_to? :name
+          rescue
+            Log.exception $!
+          end
+
+          [k,values]
+        end
+      when :double
+        fields = tsv.fields.dup if tsv.fields
+        TSV.traverse tsv, :into => named do |k,values_list|
+          k = k.first if Array === k
+          k = Misc.prepare_entity(k, tsv.key_field) if tsv.key_field
+          k = k.name if k.respond_to? :name
+
+          if fields
+            i = 0
+            new_value_list = values_list.collect do |values|
+              begin
+                values = Misc.prepare_entity(values, fields[i]) 
+                if values.respond_to? :name
+                  values.name
+                else
+                  values
+                end
+              ensure
+                i += 1
+              end
+            end
+            [k,new_value_list]
+          else
+            [k,values_list]
+          end
         end
       end
     end
